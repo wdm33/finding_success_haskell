fa367a48cfc682c04812d781e8369d36c395bd58
wdm33 <wdm33@protonmail.com>
2022-12-15 20:50:41 +0100

    backup

--- chapter4/validation-book/src/Main.hs
+++ chapter4/validation-book/src/Main.hs
@@ -54,4 +54,71 @@ validatePasswordm xs =
     >>= requireAlphaNum
     >>= checkPasswordLength
 
+{-Page 62-}
+
+reverseLine :: IO ()
+reverseLine = getLine >>= (print . reverse)
+
+reverseLinem =
+  do
+    x <- getLine
+    print $ reverse x
+
+{-continue with page 63 exercise 11 -}    
+
+{-Chapter 5 -}
+
+
+
+checkPasswordLength2 :: String -> Either String String
+checkPasswordLength2 password =
+  case (length password > 20) of
+    True -> Left "Your password cannot be longer \
+                  \than 20 characters."
+    False -> Right password
+
+
+requireAlphaNum2 :: String -> Either String String
+requireAlphaNum2 xs =
+  case (all isAlphaNum xs) of
+    False -> Left "Your password is not \
+                  \alphnumeric."
+    True -> Right xs 
+
+cleanWhitespace2 :: String -> Either String String
+cleanWhitespace2 "" = Left "Not clean, Not clean!" 
+cleanWhitespace2 (x : xs) =
+  case (isSpace x) of 
+    True -> cleanWhitespace2 xs
+    False -> Right (x : xs)
+
+validatePassword2 :: String -> Either String String
+validatePassword2 password =
+  cleanWhitespace2 password
+    >>= requireAlphaNum2
+    >>= checkPasswordLength2
+
+printTestResult :: Either String () -> IO ()
+printTestResult r =
+  case r of
+    Left err -> putStrLn err
+    Right () -> putStrLn "All tests passed."
+
+eq :: (Eq a, Show a) => Int -> a -> a -> Either String ()
+eq n actual expected =
+  case (actual == expected ) of
+    True -> Right ()
+    False -> Left (unlines
+      [ "Test " ++ show n
+      , " Expected: " ++ show expected
+      , " But got:  " ++ show actual
+      ])
+
+test :: IO ()
+test = printTestResult $
+  do
+    eq 1 (checkPasswordLength2 "")(Right "")
+    eq 2 (checkPasswordLength2 "julielovesbooks")(Right "julielovesbooks")
+    eq 3 (checkPasswordLength2 "julielovesbooks")(Right "julielovesbooks")
+
 
